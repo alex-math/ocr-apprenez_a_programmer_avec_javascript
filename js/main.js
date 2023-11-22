48aaeb472a2419bb54e1b26698172a57dffeefc3
MATHONNEAU ALEXANDRE <GA05638@MAAFPROD.E-CORAIL.COM>
2023-11-22 16:14:03 +0100

    Proposition de plusieurs listes à saisir : mots ou phrases

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 let score =0
+let saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
+let choixListeUtilisateur
 
-let motUtilisateur = prompt(listeMots[0])
-console.log(motUtilisateur)
+while(saisieUtilisateur !== "MOT" && saisieUtilisateur !== "PHRASE") {
+  saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
+}
+
+switch(saisieUtilisateur) {
+  case "MOT":
+    // copie par valeur
+    choixListeUtilisateur = [...listeMots]
+    break
+  case "PHRASE":
+    // copie par référence
+    choixListeUtilisateur = [...listePhrases]
+    break
+}
+
+for (i=0; i<choixListeUtilisateur.length; i++) {
+  let saisieUtilisateur = prompt(choixListeUtilisateur[i])
+  console.log(saisieUtilisateur)
+
+  if (saisieUtilisateur === choixListeUtilisateur[i]) {
+    console.log("Bravo !")
+    score++
+  }
+}
+
+console.log(score)
