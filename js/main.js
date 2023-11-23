867af7651d21c9ad682033e2794ed0c3c59b6c62
MATHONNEAU ALEXANDRE <GA05638@MAAFPROD.E-CORAIL.COM>
2023-11-23 13:18:15 +0100

    refactoring des fonctionnalité en function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,46 @@
-let score =0
-let saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
-let choixListeUtilisateur
-
-while(saisieUtilisateur !== "MOT" && saisieUtilisateur !== "PHRASE") {
-  saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
-}
-
-switch(saisieUtilisateur) {
-  case "MOT":
-    // copie par valeur
-    choixListeUtilisateur = [...listeMots]
-    break
-  case "PHRASE":
-    // copie par référence
-    choixListeUtilisateur = [...listePhrases]
-    break
-}
-
-for (i=0; i<choixListeUtilisateur.length; i++) {
-  let saisieUtilisateur = prompt(choixListeUtilisateur[i])
-  console.log(saisieUtilisateur)
-
-  if (saisieUtilisateur === choixListeUtilisateur[i]) {
-    console.log("Bravo !")
-    score++
+function choisirPhrasesOuMots() {
+  let saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
+  while(saisieUtilisateur !== "MOT" && saisieUtilisateur !== "PHRASE") {
+    saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
   }
+  return saisieUtilisateur
 }
 
-console.log(score)
+function lancerBoucleDeJeu(choixListeUtilisateur) {
+  let score = 0
+  for (let i=0; i<choixListeUtilisateur.length; i++) {
+    let saisieUtilisateur = prompt(choixListeUtilisateur[i])
+    console.log(saisieUtilisateur)
+    if (saisieUtilisateur === choixListeUtilisateur[i]) {
+      console.log("Bravo !")
+      score++
+    }
+  }
+  return score
+}
+
+function afficherResultat(score, nombreTotalDeProposition) {
+  console.log("Votre score est de " + score + " sur " + nombreTotalDeProposition)
+}
+
+function lancerJeu() {
+  let saisieUtilisateur = choisirPhrasesOuMots()
+  let nombreTotalDeProposition = 0
+  let score = 0
+  let choixListeUtilisateur
+
+  switch(saisieUtilisateur) {
+    case "MOT":
+      // copie par valeur
+      choixListeUtilisateur = [...listeMots]
+      break
+    case "PHRASE":
+      // copie par référence
+      choixListeUtilisateur = [...listePhrases]
+      break
+  }
+  score = lancerBoucleDeJeu(choixListeUtilisateur)
+  afficherResultat(score, choixListeUtilisateur.length)
+}
+
+lancerJeu()
