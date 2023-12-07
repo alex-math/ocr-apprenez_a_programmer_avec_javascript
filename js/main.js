15df0efbbe63dfad0782b6ef7c749816d9ce317c
MATHONNEAU ALEXANDRE <GA05638@MAAFPROD.E-CORAIL.COM>
2023-12-07 11:34:34 +0100

    Affichage console de la saisie utilisateur dans le champ input

--- js/main.js
+++ js/main.js
@@ -4,17 +4,4 @@
  *
  *********************************************************************************/
 
-// lancerJeu()
-
-let inputEcriture = document.getElementById("inputEcriture");
-console.log(inputEcriture);
-let boutonValider = document.getElementById("btnValiderMot");
-console.log(boutonValider);
-
-let zoneProposition = document.querySelector(".zoneProposition");
-console.log(zoneProposition);
-let zoneScore = document.querySelector(".zoneScore span");
-console.log(zoneScore);
-
-let listeInputRadio = document.querySelectorAll(".optionSource input");
-console.log(listeInputRadio);
+lancerJeu()
--- js/scripts.js
+++ js/scripts.js
@@ -4,53 +4,26 @@
  *
  *********************************************************************************/
 
-/**
- *
- * @returns {string}
- */
-function choisirPhrasesOuMots() {
-  let saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
-  while(saisieUtilisateur !== "MOT" && saisieUtilisateur !== "PHRASE") {
-    saisieUtilisateur = prompt("Voulez-vous saisir un MOT ou une PHRASE ?")
-  }
-  return saisieUtilisateur
-}
-
-function lancerBoucleDeJeu(choixListeUtilisateur) {
-  let score = 0
-  for (let i=0; i<choixListeUtilisateur.length; i++) {
-    let saisieUtilisateur = prompt(choixListeUtilisateur[i])
-    console.log(saisieUtilisateur)
-    if (saisieUtilisateur === choixListeUtilisateur[i]) {
-      console.log("Bravo !")
-      score++
-    }
-  }
-  return score
-}
-
 function afficherResultat(score, nombreTotalDeProposition) {
-  console.log("Votre score est de " + score + " sur " + nombreTotalDeProposition)
+  let zoneScore = document.querySelector(".zoneScore span")
+  let scoreAAfficher = `${score} / ${nombreTotalDeProposition}`
+  zoneScore.innerText = scoreAAfficher
 }
 
 function lancerJeu() {
-          let baliseZonePropositionSpan = document.querySelector('#zoneProposition span');
-  console.log(baliseZonePropositionSpan);
-  let saisieUtilisateur = choisirPhrasesOuMots()
-  let nombreTotalDeProposition = 0
-  let score = 0
-  let choixListeUtilisateur
 
-  switch(saisieUtilisateur) {
-    case "MOT":
-      // copie par valeur
-      choixListeUtilisateur = [...listeMots]
-      break
-    case "PHRASE":
-      // copie par référence
-      choixListeUtilisateur = [...listePhrases]
-      break
-  }
-  score = lancerBoucleDeJeu(choixListeUtilisateur)
-  afficherResultat(score, choixListeUtilisateur.length)
+  let baliseZonePropositionSpan = document.querySelector('#zoneProposition span');
+  console.log(baliseZonePropositionSpan);
+
+  let score = 0
+  let nbMotProposes = 0
+
+  let btnValiderMot = document.getElementById("btnValiderMot")
+  let inputEcriture = document.getElementById("inputEcriture");
+
+  btnValiderMot.addEventListener("click", () => {
+    console.log(inputEcriture.value)
+  })
+
+  afficherResultat(score, nbMotProposes)
 }
